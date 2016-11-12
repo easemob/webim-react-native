cc98b0c06d330127a5355766936374a5d1f4d817
wytheme <liu48566203@gmail.com>
2016-11-12 11:59:53 +0800

    add demo code

---
+++ reactnative/App/Navigation/NavItems.js
@@ -0,0 +1,42 @@
+// @flow
+
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import styles from './Styles/NavItemsStyle'
+import { Actions as NavigationActions } from 'react-native-router-flux'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { Colors, Metrics } from '../Themes'
+
+const openDrawer = () => {
+  NavigationActions.refresh({
+    key: 'drawer',
+    open: true
+  })
+}
+
+export default {
+  backButton () {
+    return (
+      <TouchableOpacity onPress={NavigationActions.pop}>
+        <Icon name='angle-left'
+          size={Metrics.icons.medium}
+          color={Colors.snow}
+          style={styles.navButtonLeft}
+        />
+      </TouchableOpacity>
+    )
+  },
+
+  hamburgerButton () {
+    return (
+      <TouchableOpacity onPress={openDrawer}>
+        <Icon name='bars'
+          size={Metrics.icons.medium}
+          color={Colors.snow}
+          style={styles.navButtonLeft}
+        />
+      </TouchableOpacity>
+    )
+  }
+
+}
---
+++ reactnative/App/Navigation/Styles/NavItemsStyle.js
@@ -0,0 +1,12 @@
+// @flow
+
+import {StyleSheet} from 'react-native'
+import { Metrics, Colors } from '../../Themes/'
+
+export default StyleSheet.create({
+  navButtonLeft: {
+    marginLeft: Metrics.baseMargin,
+    backgroundColor: Colors.transparent,
+    width: Metrics.icons.medium
+  }
+})
