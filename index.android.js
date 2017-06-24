e8d22093ce5c996ee31326f8eddd8379f6e52d2f
sunny20131314 <the_sunny2013@163.com>
2017-06-24 14:39:58 +0800

    修复运行项目时报错：Cannot read property 'style' of undefined

--- index.android.js
+++ index.android.js
@@ -1,7 +1,12 @@
 // @flow
 
 import './App/Config/ReactotronConfig'
-import { AppRegistry } from 'react-native'
-import App from './App/Containers/App'
+import ReactNative, { AppRegistry, View } from 'react-native'
+
+// 兼容 0.39 版本的支持
+ReactNative.ViewPropTypes || ( ReactNative.ViewPropTypes = View.propTypes );
+ReactNative.BackHandler || ( ReactNative.BackHandler = ReactNative.BackAndroid );
+
+let App = require( './App/Containers/App' ).default;
 
 AppRegistry.registerComponent('app', () => App)
